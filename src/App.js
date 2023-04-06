64d31f1e4fda5f123744fead08b7cab30c524e73
Nader Nawfal <nader.nauf@gmail.com>
2023-04-06 13:46:33 +0300

    Fixing Component reference

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import { useState } from "react";
 import DropDown from "./components/dropdown";
+import FilterDemo from "./components/dropdown";
 
 function App() {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (bicep) => {
-    setSelection(bicep);
-  };
-
-  const biceps = [
-    { label: "Biceps Curls", value: "BC" },
-    { label: "Incline Dumbbell Curls", value: "IDC" },
-    { label: "Reverse Barbell Curls", value: "RBC" },
-    { label: "Standing Hammer Curl", value: "SHC" },
-  ];
-  return (
-    <DropDown biceps={biceps} selection={selection} onSelect={handleSelect} />
-  );
+  return <FilterDemo />;
 }
 
 export default App;
