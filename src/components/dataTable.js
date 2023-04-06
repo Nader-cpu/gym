3e4ec393e6e6088ffb6b0e7d2f16d06023a32c6a
Kifah Nasrldeen <kifahnn1122@gmail.com>
2023-04-06 15:43:27 +0300

    Table setup

--- src/components/dataTable.js
+++ src/components/dataTable.js
@@ -4,7 +4,7 @@ import { Column } from "primereact/column";
 
 export default function GridLinesDemo() {
 	const [products, setProducts] = useState([
-		{ code: "dddssa", name: "Walid", category: "kifah", quantity: "brahim" },
+		{ name: "pullups", MuscleGroup: "back" },
 	]);
 
 	useEffect(() => {}, []);
@@ -15,10 +15,10 @@ export default function GridLinesDemo() {
 				value={products}
 				showGridlines
 				tableStyle={{ margin: "auto", width: "75vw" }}>
-				<Column field="code" header="Code"></Column>
 				<Column field="name" header="Name"></Column>
-				<Column field="category" header="Category"></Column>
-				<Column field="quantity" header="Quantity"></Column>
+				<Column field="muscleGroup" header="MuscleGroup"></Column>
+				<Column field="sets" header="Sets"></Column>
+				<Column field="action" header="Action"></Column>
 			</DataTable>
 		</div>
 	);
