4f27e6f9b1f2554cead9b051f6b997d56158f7ca
Kifah Nasrldeen <kifahnn1122@gmail.com>
2023-04-06 14:33:31 +0300

    basic table setup

--- src/components/dataTable.js
+++ src/components/dataTable.js
@@ -1,11 +1,25 @@
+import React, { useState, useEffect } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 
-<DataTable value={products} stripedRows tableStyle={{ minWidth: "50rem" }}>
-	<Column field="code" header="Code"></Column>
-	<Column field="name" header="Name"></Column>
-	<Column field="category" header="Category"></Column>
-	<Column field="quantity" header="Quantity"></Column>
-</DataTable>;
+export default function GridLinesDemo() {
+	const [products, setProducts] = useState([
+		{ code: "dddssa", name: "Walid", category: "kifah", quantity: "brahim" },
+	]);
 
-export default DataTable;
+	useEffect(() => {}, []);
+
+	return (
+		<div className="card">
+			<DataTable
+				value={products}
+				showGridlines
+				tableStyle={{ margin: "auto", width: "75vw" }}>
+				<Column field="code" header="Code"></Column>
+				<Column field="name" header="Name"></Column>
+				<Column field="category" header="Category"></Column>
+				<Column field="quantity" header="Quantity"></Column>
+			</DataTable>
+		</div>
+	);
+}
